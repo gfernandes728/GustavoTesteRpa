17cc2e993b9ab4def296ffa0beb58ed8d1e8b72a
gfernandes728 <guga.728@gmail.com>
2026-03-18 15:25:46 -0300

    Logs e informações/erros para falhas ao pegar dados da cotação

--- RpaWorker/Services/ScrapingService.cs
+++ RpaWorker/Services/ScrapingService.cs
@@ -16,22 +16,47 @@ public class ScrapingService
         var response = await httpClient.GetAsync("https://api.exchangerate-api.com/v4/latest/USD");
 
         response.EnsureSuccessStatusCode();
+        if (!response.IsSuccessStatusCode)
+        {
+            logger.LogError($"Falha ao capturar valor da Api [https://api.exchangerate-api.com/v4/latest/USD] {DateTime.UtcNow}.");
+            return;
+        }
 
         var json = await response.Content.ReadAsStringAsync();
-        var data = JsonDocument.Parse(json);
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            logger.LogError($"Sem dados capturados da Api [https://api.exchangerate-api.com/v4/latest/USD] {DateTime.UtcNow}.");
+            return;
+        }
 
-        var value = data.RootElement
-            .GetProperty("rates")
-            .GetProperty("BRL")
-            .GetDecimal();
+        var value = GetValue(data: JsonDocument.Parse(json));
+        if (!value.HasValue)
+        {
+            logger.LogError($"Sem valores de USD para BRL, capturados da Api [https://api.exchangerate-api.com/v4/latest/USD] {DateTime.UtcNow}.");
+            return;
+        }
 
         logger.LogInformation($"Capturado Valor {value} - {DateTime.UtcNow}");
 
-        _dataStore.Add(new() { Value = value });
+        _dataStore.Add(new() { Value = value.Value });
 
         logger.LogInformation($"Adicionado Valor {value} - {DateTime.UtcNow}");
         logger.LogInformation($"Total capturado: {_dataStore.Count}");
     }
 
     public IEnumerable<Quote> GetAll() => _dataStore;
+
+    private decimal? GetValue(JsonDocument? data)
+    {
+        if  (
+                data is not null &&
+                data.RootElement.TryGetProperty("rates", out var rates) &&
+                rates.TryGetProperty("BRL", out var valueBrl)
+            )
+        {
+            return valueBrl.GetDecimal();
+        }
+
+        return null;
+    }
 }
